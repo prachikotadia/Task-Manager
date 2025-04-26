6d0883139006577a66b4e61e49410c420b6dca71
PFA23SCM27K <pkotadia@hawk.iit.edu>
2025-04-26 03:31:50 -0500

    Connected PostgreSQL database to task-service

--- backend/task-service/main.py
+++ backend/task-service/main.py
@@ -5,8 +5,10 @@ from typing import List
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
+import os
 
-DATABASE_URL = "postgresql://task_manager_db_d9cz_user:NZYbPSCUfLL4YmYMH3EVkySstd8Rx6X8@dpg-d066fbali9vc73e20nhg-a/task_manager_db_d9cz"
+DATABASE_URL = os.getenv("DATABASE_URL","postgresql://task_manager_db_d9cz_user:NZYbPSCUfLL4YmYMH3EVkySstd8Rx6X8@dpg-d066fbali9vc73e20nhg-a/task_manager_db_d9cz")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -33,6 +35,7 @@ app.add_middleware(
 class Task(BaseModel):
     title: str
     description: str
+
 
 @app.post("/tasks")
 def create_task(task: Task):
